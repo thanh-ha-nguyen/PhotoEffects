e3c4b4b7c7fb11f867b0582340ac81aa8e975edd
Thanh Ha Nguyen <thanh-ha.nguyen@myy.haaga-helia.fi>
2026-02-18 21:23:56 +0200

    Add bottom tab navigator.

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="cog" color={color} size={28} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
