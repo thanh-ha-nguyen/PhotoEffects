ff639295b9f2b5d056ab0536d6ed0d81abfbb43c
Thanh Ha Nguyen <thanh-ha.nguyen@myy.haaga-helia.fi>
2026-02-18 20:47:29 +0200

    Add Inter font from Google Fonts.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from "expo-router";
+import {
+  Inter_100Thin,
+  Inter_100Thin_Italic,
+  Inter_400Regular,
+  Inter_400Regular_Italic,
+  Inter_700Bold,
+  Inter_700Bold_Italic,
+} from "@expo-google-fonts/inter";
+import { useFonts } from "@expo-google-fonts/inter/useFonts";
+import { Slot, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [isReady] = useFonts({
+    "Inter Thin": Inter_100Thin,
+    "Inter Regular": Inter_400Regular,
+    "Inter Bold": Inter_700Bold,
+    "Inter Thin Italic": Inter_100Thin_Italic,
+    "Inter Regular Italic": Inter_400Regular_Italic,
+    "Inter Bold Italic": Inter_700Bold_Italic,
+  });
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  return <Slot />;
 }
